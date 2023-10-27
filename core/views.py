011079b35f635f3df534e5e25634bec0ed304a3a
Degisew Mengist <jegisew21@gmail.com>
2023-10-27 16:33:11 +0300

    Implement Class based view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.views.generic import CreateView
 from django.shortcuts import render
+from .models import SourceData 
+from .form import SourceDataForm
 
-# Create your views here.
+
+class SourceDataView(CreateView):
+    model = SourceData
+    form_class = SourceDataForm()
+    template_name = 'core/form.html'
+    success_url = 'url name here'
+    # context_object_name = ''  Default name is 'form'
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        
+        # Data processing logic goes here...
+
+        return super().form_valid(form)
+

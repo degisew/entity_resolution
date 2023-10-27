52caae396d9036a8fdfec4821b0e6153514f90f2
Degisew Mengist <jegisew21@gmail.com>
2023-10-27 11:08:10 +0300

    Migrations

--- core/models.py
+++ core/models.py
@@ -17,5 +17,21 @@ class ReferenceData(models.Model):
     birth_date = models.DateTimeField()
     gender = models.CharField(max_length=255,choices=GENDER_CHOICES)
 
+class SourceData(models.Model):
+    MALE = 'Male'
+    FEMALE = 'Female'
+    GENDER_CHOICES = [
+        ('male', MALE),
+        ('female', FEMALE)
+    ]
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=200)
+    address = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    birth_date = models.DateTimeField()
+    gender = models.CharField(max_length=255,choices=GENDER_CHOICES)
 
 

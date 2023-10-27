a837e92281be5ce96885ac6b6d8f3b26465a88cd
Degisew Mengist <jegisew21@gmail.com>
2023-10-27 16:34:51 +0300

    Render form

--- core/form.py
+++ core/form.py
@@ -5,3 +5,4 @@ class SourceDataForm(ModelForm):
     class Meta:
         model = SourceData
         fields = ['first_name', 'middle_name', 'last_name', 'gender', 'email', 'phone', 'address', 'location', 'birth_date']
+        

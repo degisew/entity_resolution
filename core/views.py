bb7af3779958ec199024c80145e96bf304013dfb
Degisew Mengist <jegisew21@gmail.com>
2023-10-27 16:44:11 +0300

    Added a HomeView

--- core/views.py
+++ core/views.py
@@ -1,16 +1,21 @@
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.views.generic import CreateView
-from django.shortcuts import render
+from django.views import View
 from .models import SourceData 
 from .form import SourceDataForm
 
+
+
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse('Home')
 
 class SourceDataView(CreateView):
     model = SourceData
     form_class = SourceDataForm()
     template_name = 'core/form.html'
-    success_url = 'url name here'
+    success_url = 'create-data'
     # context_object_name = ''  Default name is 'form'
 
     def form_valid(self, form: BaseModelForm) -> HttpResponse:

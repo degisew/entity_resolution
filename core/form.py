150b70f10cbf2caf125f0ca80a7a84f951792c6e
Degisew Mengist <jegisew21@gmail.com>
2023-10-27 17:05:00 +0300

    Add a radio button widget

--- core/form.py
+++ core/form.py
@@ -1,7 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from .models import SourceData
 
-class SourceDataForm(ModelForm):
+class SourceDataForm(forms.ModelForm):
+    MALE = 'Male'
+    FEMALE = 'Female'
+    GENDER_CHOICES = [
+        ('male', MALE),
+        ('female', FEMALE)
+    ]
+    gender = forms.ChoiceField(
+        widget=forms.RadioSelect,
+        choices=GENDER_CHOICES
+    )
     class Meta:
         model = SourceData
         fields = ['first_name', 'middle_name', 'last_name', 'gender', 'email', 'phone', 'address', 'location', 'birth_date']

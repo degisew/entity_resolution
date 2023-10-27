9303635693ecc6e37042cc9615daf611fe4a854a
Degisew Mengist <jegisew21@gmail.com>
2023-10-27 09:38:41 +0300

    Add secret key to enironment file

--- entityResolution/settings.py
+++ entityResolution/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$6c0ha1kfsqnk2r9g3vykcs9t-9w*i7bsj306#nc9r2@xtt)4!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

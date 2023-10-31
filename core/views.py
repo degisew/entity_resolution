5ded306f827ec3ded0486f994294c95dd1fb9dc9
Degisew Mengist <jegisew21@gmail.com>
2023-10-31 14:12:01 +0300

    Added a cleaning logic to form values

--- core/views.py
+++ core/views.py
@@ -27,6 +27,12 @@ class SourceDataView(CreateView):
         first_name = re.sub('[\W_]+', '',cleaned_data['first_name'].strip().lower())
         middle_name = re.sub('[\W_]+', '',cleaned_data['middle_name'].strip().lower())
         last_name = re.sub('[\W_]+', '',cleaned_data['last_name'].strip().lower())
+        email = re.sub('[\W_]+', '',cleaned_data['email'].strip().lower())
+        phone = re.sub('[\d_]+', '',cleaned_data['phone'].strip())
+        location = re.sub('[\W_]+', '',cleaned_data['location'].strip().lower())
+        address = re.sub('[\W_]+', '',cleaned_data['address'].strip().lower())
+        birth_date = re.sub('[\W_]+', '',cleaned_data['birth_date'].strip().lower())
+        gender = re.sub('[\W_]+', '',cleaned_data['gender'].strip().lower())
 
 
     def form_valid(self, form: BaseModelForm):

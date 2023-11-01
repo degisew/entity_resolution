3ccf2fac24e4e4a760ca35f0b53aa29f77511f3b
Degisew Mengist <jegisew21@gmail.com>
2023-11-01 11:01:50 +0300

    Add a data cleaning logic

--- core/views.py
+++ core/views.py
@@ -17,26 +17,21 @@ class SourceDataView(CreateView):
     form_class = SourceDataForm
     template_name = 'core/form.html'
     success_url = 'home'
-    # cleaned_data = "  abcjw:, .@! EiwYQA                                "
-    # first_name = re.sub('[\W_]+', '', cleaned_data.lower())
-    # print("##########################",first_name,"##########################")
-
-    # context_object_name = ''  Default name is 'form'
-
+    birth_date = re.sub('[a-zA-Z_.@\#\$\%\^\&\*\~]', '', '12.%@#/3fklfgk~4/_42')
+    print(birth_date)
     def process_data(self, cleaned_data):
         first_name = re.sub('[\W_]+', '',cleaned_data['first_name'].strip().lower())
         middle_name = re.sub('[\W_]+', '',cleaned_data['middle_name'].strip().lower())
         last_name = re.sub('[\W_]+', '',cleaned_data['last_name'].strip().lower())
-        email = re.sub('[\W_]+', '',cleaned_data['email'].strip().lower())
-        phone = re.sub('[\d_]+', '',cleaned_data['phone'].strip())
+        #email = cleaned_data['email'].strip().lower()
+        phone = re.sub('[\D_]+', '',cleaned_data['phone'].strip())
         location = re.sub('[\W_]+', '',cleaned_data['location'].strip().lower())
         address = re.sub('[\W_]+', '',cleaned_data['address'].strip().lower())
-        birth_date = re.sub('[\W_]+', '',cleaned_data['birth_date'].strip().lower())
+        birth_date = re.sub('[a-zA-Z_\.\@\#\$\%\^\&\*\~]', '',cleaned_data['birth_date'].strip().lower())
         gender = re.sub('[\W_]+', '',cleaned_data['gender'].strip().lower())
 
 
     def form_valid(self, form: BaseModelForm):
-        
         # Data processing logic goes here...
         return super().form_valid(form)
 

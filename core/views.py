2428237f03785de07664e66e28ab11e566aca560
Degisew Mengist <jegisew21@gmail.com>
2023-11-02 10:04:48 +0300

    override save method

--- core/views.py
+++ core/views.py
@@ -3,12 +3,14 @@ from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.views.generic import CreateView
 from django.views import View
-from .models import SourceData 
+from .models import SourceData
 from .form import SourceDataForm
+
 
 class HomeView(View):
     def get(self, request):
         return HttpResponse('Home')
+
 
 class SourceDataView(CreateView):
     model = SourceData
@@ -16,20 +18,39 @@ class SourceDataView(CreateView):
     template_name = 'core/form.html'
     success_url = 'home'
 
-    
     def process_data(self, cleaned_data):
-        first_name = re.sub(r'[\W_]+', '',cleaned_data['first_name'].strip().lower())
-        middle_name = re.sub(r'[\W_]+', '',cleaned_data['middle_name'].strip().lower())
-        last_name = re.sub(r'[\W_]+', '',cleaned_data['last_name'].strip().lower())
+        first_name = re.sub(
+            r'[\W\d_\\\.@#$%^&*~]+', '', cleaned_data['first_name'].strip().lower())
+        middle_name = re.sub(
+            r'[\W\d_\\\.@#$%^&*~]+', '', cleaned_data['middle_name'].strip().lower())
+        last_name = re.sub(
+            r'[\W\d_\\\.@#$%^&*~]+', '', cleaned_data['last_name'].strip().lower())
         email = cleaned_data[r'email'].strip().lower()
-        phone = re.sub(r'[\D_]+', '',cleaned_data['phone'].strip())
-        location = re.sub(r'[\W_]+', '',cleaned_data['location'].strip().lower())
-        address = re.sub(r'[\W\d_,[\]./#]+', '',cleaned_data['address'].strip().lower())
-        birth_date = re.sub(r'[a-zA-Z_\.@#$%^&*~]', '',cleaned_data['birth_date'].strip().lower())
-        gender = re.sub(r'[\d_\\\.@#$%^&*~]+', '',cleaned_data['gender'].strip().lower())
+        phone = re.sub(r'[\D_]+', '', cleaned_data['phone'].strip())
+        location = re.sub(r'[\W\d_\\\.@#$%^&*~]+', '',
+                          cleaned_data['location'].strip().lower())
+        address = re.sub(r'[\W\d_,[\]./#]+', '',
+                         cleaned_data['address'].strip().lower())
+        birth_date = re.sub(r'[a-zA-Z_\.@#$%^&*~]', '',
+                            str(cleaned_data['birth_date']))
+        gender = re.sub(r'[\d_\\\.@#$%^&*~]+', '',
+                        cleaned_data['gender'].strip().lower())
 
+        return SourceData(
+            first_name=first_name,
+            middle_name=middle_name,
+            last_name=last_name, 
+            phone=phone,
+            email=email,
+            location=location, 
+            address=address,
+            gender=gender,
+            birth_date=birth_date,
+        )
 
     def form_valid(self, form: SourceDataForm):
-        print("$$$$$$$$$$$$$$$$$", form.cleaned_data, "$$$$$$$$$$$$$$$$$")
-        self.process_data(form.cleaned_data)
-        return super().form_valid(form)
+        processed_data = self.process_data(form.cleaned_data)
+        print(processed_data)
+        processed_data.save()
+        return SourceDataForm()
+        # return super().form_valid(form)

3c219b7c0abcecec58c6fbaee7a78ca2c891c03f
Degisew Mengist <jegisew21@gmail.com>
2023-11-02 10:03:33 +0300

    Added a form method

--- core/form.py
+++ core/form.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from .models import SourceData
 
@@ -23,3 +24,18 @@ class SourceDataForm(forms.ModelForm):
         model = SourceData
         fields = ['first_name', 'middle_name', 'last_name', 'gender', 'email', 'phone', 'address', 'location', 'birth_date']
         
+    
+    # def save(self, commit: bool =True):
+    #     instance = super(SourceDataForm, self).save(commit=False)
+    #     instance.first_name = self.cleaned_data.get('first_name')
+    #     instance.middle_name = self.cleaned_data.get('middle_name')
+    #     instance.last_name = self.cleaned_data.get('last_name')
+    #     instance.phone = self.cleaned_data.get('phone')
+    #     instance.email = self.cleaned_data.get('email')
+    #     instance.location = self.cleaned_data.get('location')
+    #     instance.gender = self.cleaned_data.get('gender')
+    #     instance.address = self.cleaned_data.get('address')
+    #     instance.birth_date = self.cleaned_data.get('birth_date')
+    #     if commit:
+    #         instance.save()
+    #     return instance
